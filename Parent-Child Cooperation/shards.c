af7b17aaa215e4383ea3537a01856d9a3f0e620c
uriah2006 <uriah2006@gmail.com>
2013-11-19 12:21:38 -0500

    not working.
    building the array is working

---
+++ Parent-Child Cooperation/shards.c
@@ -0,0 +1,37 @@
+//test
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define  M 800
+
+void toArray1D(int array[M],char file_name[30]);
+
+int main(){
+	int i;
+	char file_name[30];							//file name
+	int array[M]={0};
+	
+	printf("Type in the name of the file containing the numbers:");
+	scanf("%s",file_name);
+	
+	toArray1D(array,file_name);
+	for (i=0; i<M;i++){
+		printf("%d %d \n",i,array[i]);
+		
+	}
+
+}
+void toArray1D(int array[M],char file_name[30]){
+	FILE *fptr = fopen(file_name,"r");
+	int num;
+	int i = 0;
+	
+	fscanf (fptr, "%d", &num);    
+
+	while (!feof (fptr)){  
+		array[i]=num;
+		i++;
+		fscanf (fptr, "%d", &num);      
+    }
+	fclose (fptr);        
+}
