d885382eeaf1fe2b82d4a7460bf470dd07cf92ea
uriah2006 <uriah2006@gmail.com>
2013-12-04 19:03:26 -0500

    i am about to change alot of stuff

--- Parent-Child Cooperation/assigment3.c
+++ Parent-Child Cooperation/assigment3.c
@@ -3,6 +3,7 @@ cs 350 10/18/2013
 
 */
 #include <stdio.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
@@ -10,23 +11,25 @@ cs 350 10/18/2013
 #define ARRAY_SIZE( array ) sizeof( array ) / sizeof( array[0] )
 
 void toArray1D(int array[M],char file_name[30]);
-int * recursiveProcessTree(int array[M],int size);
+void recursiveProcessTree(int array[M],int start, int end);
+int comp(const int * a,const int * b);
 int main(){
 	//decoration	
-	char file_name[30];							//file name
+	char file_name[30]="test2.txt";							//file name
 	int array[M]={0};
 	int i=0;
 	
 	//get data
-	printf("Type in the name of the file containing the text:");
-	scanf("%s",file_name);
+	//printf("Type in the name of the file containing the text:");
+	//scanf("%s",file_name);
 	
-	//
-	
+	//fills a one dimensional array with the contents from a file
 	toArray1D(array,file_name);
-
-	alarm(5);
-	recursiveProcessTree(array,(sizeof(array)/4));
+	
+	//error catch
+	//alarm(5);
+	
+	recursiveProcessTree(array,0,(sizeof(array)/4));
 	
 	i++;
 	printf ("%d \n",i);
@@ -53,82 +56,66 @@ void toArray1D(int array[M],char file_name[30]){
 	fclose (fptr);        
 }
 
-int * recursiveProcessTree(int array[M],int size){
+void recursiveProcessTree(int array[M],int start, int end){
 	//decoration
 	int i=0, j=0, k=0;												//i j and k are used for sorting
-	int left[size/2], right[size/2]; 
-	memcpy(left, array, size/2); 
-	memcpy(right, &array[size/2], size); 
 	pid_t child;
 	
 	
 	// if you need to recurs
-    if (size>100){
+    if ((end-start)>100){
 	
 		//fork it!!
+		printf("child not born yet\n");
 		child=fork();
-		if (child < 0){
-			printf("failed to fork");
-			exit(-1);
+		if (child == -1){
+			perror("failed to fork");
 		}
-		
-		
 		
 		//child go left
-		if (child==0){
-			recursiveProcessTree(left,sizeof(left)/4);
-		}
-		//parent go right
-		else{
-			wait(NULL);
-			recursiveProcessTree(right,sizeof(right)/4);
+		else if (child < 0){
+			recursiveProcessTree(array,start,(end/2)-1 );
+			printf("child done\n");
 		}
 		
-		/*trying to join every thing back up **************************************
-		while (child = waitpid(-1, NULL, 0)) {
-			if (errno == ECHILD) {
-				break;
-			}
+		//parent go right
+		else{
+			recursiveProcessTree(array, end/2 ,end);
 		}
+		/*trying to join every thing back up **************************************
 		*/
 	}
     // time to sort and return
 	// TODO
 	//sort(array);
-    while(k<size){
-		if(left[i] < right[j]){
-			array[k] = left[i];
-			i++;
-		}
-		else{
-			array[k] = right[j];
-			j++;
-		}
-		k++;
-	}
-	return array;
+    qsort(array,(end-start),sizeof(array[start]),comp) ;
 }
 
+//got it from http://cplus.about.com/od/learningc/ss/pointers2_8.htm
+int comp(const int * a,const int * b)
+{
+  if (*a==*b)
+    return 0;
+  else
+    if (*a < *b)
+        return -1;
+     else
+      return 1;
+}
 /*
-pid_t child_pid;
-   int i;
+int main()
+{
+    pid_t b; // note that the actual return type of fork is 
+             // pid_t, though it's probably just an int typedef'd or macro'd
 
-   child_pid=fork();
-   if (child_pid<0)  
-    { printf("Failed to fork\n"); exit(-1); } 
+    b = fork();
+    if (b == -1) perror("Fork failed");
+    else if (b > 0) {
+        printf("My name is Manish\n"); // parent process
+    else
+        printf("My name is Anil\n"); // child process
 
-   if (child_pid==0) //child code  
-    for (i=0; i<5; i++)
-           printf("i=%d, pid=%ld\n",i,(long)getpid());        
-   else {//parent code
-     wait(NULL);
-    for (i=5; i<10; i++)
-           printf("i=%d, pid=%ld\n",i,(long)getpid());        
-   }
-
-   //child & parent
-   printf("Process %ld, parent %ld \n",(long)getpid(),(long)getppid());
-
-   return 0;
+    printf("The value of b is %d\n", b);
+    return 0;
 }
 */
