6d05b503bf087e86acda9f751ea7db68de4aaace
uriah2006 <uriah2006@gmail.com>
2013-12-05 10:14:12 -0500

    working. not recursive any more
    not sorting in the child
    not merging on the way back up

--- Parent-Child Cooperation/assigment3.c
+++ Parent-Child Cooperation/assigment3.c
@@ -2,49 +2,50 @@
 cs 350 10/18/2013
 
 */
+#include <stdlib.h>
 #include <stdio.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <errno.h>
+#include <signal.h>  // for line 97
 #define  M 800
-#define ARRAY_SIZE( array ) sizeof( array ) / sizeof( array[0] )
 
 void toArray1D(int array[M],char file_name[30]);
-void recursiveProcessTree(int array[M],int start, int end);
-int comp(const int * a,const int * b);
+void forkingTree(int array[M]);
+int comp(const void * a,const void * b);
 int main(){
 	//decoration	
-	char file_name[30]="test2.txt";							//file name
+	char file_name[30];									//file name
 	int array[M]={0};
 	int i=0;
 	
 	//get data
-	//printf("Type in the name of the file containing the text:");
-	//scanf("%s",file_name);
+	printf("Type in the name of the file containing the text:");
+	scanf("%s",file_name);
 	
 	//fills a one dimensional array with the contents from a file
 	toArray1D(array,file_name);
 	
-	//error catch
-	//alarm(5);
 	
-	recursiveProcessTree(array,0,(sizeof(array)/4));
+	//splits up the array and sorts it
+	forkingTree(array);
 	
-	i++;
-	printf ("%d \n",i);
-	/*for (i=0;i<sizeof(array)/4;i++){
+	
+	// pints out the sorted array
+	for (i=0;i<sizeof(array)/4;i++){
 		printf ("%d \n",array[i]);
 	}
-	*/
 	
 	
 	return 0;
 }
 
 void toArray1D(int array[M],char file_name[30]){
+	//opens file
+	
 	FILE *fptr = fopen(file_name,"r");
+	
+	//value from file
 	int num;
+	
+	//start of the array
 	int i = 0;
 	
 	fscanf (fptr, "%d", &num);    
@@ -56,66 +57,65 @@ void toArray1D(int array[M],char file_name[30]){
 	fclose (fptr);        
 }
 
-void recursiveProcessTree(int array[M],int start, int end){
+void forkingTree(int array[M]){
 	//decoration
-	int i=0, j=0, k=0;												//i j and k are used for sorting
-	pid_t child;
 	
+	//i j and k are used for sorting
+	int i=0, j=0, k=0;
+
+	//this controls how many time I am going to go through the for loop
+	int current, bottem=3;
 	
-	// if you need to recurs
-    if ((end-start)>100){
-	
-		//fork it!!
-		printf("child not born yet\n");
-		child=fork();
-		if (child == -1){
-			perror("failed to fork");
-		}
+	// whose your daddy ... 
+	pid_t kid, dad=getpid();
+
+    //for every loop it will make a fork to make a symmetrical tree
+	for(current=0; current<bottem; current++){
 		
-		//child go left
-		else if (child < 0){
-			recursiveProcessTree(array,start,(end/2)-1 );
-			printf("child done\n");
-		}
+		// FORK THE KID!!
+		kid=fork();
 		
-		//parent go right
-		else{
-			recursiveProcessTree(array, end/2 ,end);
+		//makes sure the fork .... forked
+		if (kid == -1){
+			printf("failed fork");
+			exit(1);
+		
+		// this is the parent it is essentially the left leg
+		}else if (kid > 0) {
+			printf ("going right\n");
+			continue;
+		
+		// this is the kid it is essentially the right leg
+        }else{
+			printf ("going left\n");
+			continue;
 		}
-		/*trying to join every thing back up **************************************
-		*/
 	}
-    // time to sort and return
-	// TODO
-	//sort(array);
-    qsort(array,(end-start),sizeof(array[start]),comp) ;
+	
+	// I am your daddy!!
+	if (dad != getpid()){
+		kill (getpid(), SIGUSR1);
+	}
+	
+	// daddy goes on nap time so it looks like I am sorting 
+	sleep(4);
+	
+	// remember that I made you laugh so I should get some credit
+	qsort(array,800,sizeof(array[0]),comp);
 }
 
-//got it from http://cplus.about.com/od/learningc/ss/pointers2_8.htm
-int comp(const int * a,const int * b)
+
+//got it from http://cplus.about.com/od/learningc/ss/pointers2_8.htm 
+//I knew where is was so I was not about to write it up again
+int comp(const void * a,const void * b)
 {
-  if (*a==*b)
+  int A = *((int*)a);
+  int B = *((int*)b);
+  if (A==B)
     return 0;
   else
-    if (*a < *b)
+    if (A < B)
         return -1;
      else
       return 1;
 }
-/*
-int main()
-{
-    pid_t b; // note that the actual return type of fork is 
-             // pid_t, though it's probably just an int typedef'd or macro'd
-
-    b = fork();
-    if (b == -1) perror("Fork failed");
-    else if (b > 0) {
-        printf("My name is Manish\n"); // parent process
-    else
-        printf("My name is Anil\n"); // child process
-
-    printf("The value of b is %d\n", b);
-    return 0;
-}
-*/
